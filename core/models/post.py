11ce9305397a2ce7256ba08eb17748e7d2a4178d
XAILOX1337 <olgklm07@gmail.com>
2024-10-20 15:36:14 +0300

    create post mixin

--- core/models/post.py
+++ core/models/post.py
@@ -1,21 +1,20 @@
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column
+
 from .base import Base
-from typing import TYPE_CHECKING
-from sqlalchemy import String, Text, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-
-if TYPE_CHECKING:
-    from .user import User
+from .mixins import UserRelationMixin
 
 
-class Post(Base):
+class Post(UserRelationMixin, Base):
+    # _user_id_nullable = False
+    # _user_id_unique = False
+    _user_back_populates = "posts"
+
     title: Mapped[str] = mapped_column(String(100), unique=False)
     body: Mapped[str] = mapped_column(
         Text,
-        default = "",
-        server_default = "",
+        default="",
+        server_default="",
     )
-    user_id: Mapped[int] = mapped_column(
-        ForeignKey("users.id")
-    )
-    user: Mapped["User"] = relationship(back_populates="posts")
+    
     
